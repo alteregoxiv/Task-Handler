571566bdd54a85de9dabe7f963510fd6145b1a07
alteregoxiv <prasad.biswasxiv@gmail.com>
2021-02-06 19:39:57 +0000

    Mail File Complete

--- mail.py
+++ mail.py
@@ -1,9 +1,27 @@
 """
-
+Mail to send Password
 """
 
 from os import environ 
+from smtplib import SMTP
+from passwd import genPwd
 
 email, pwd = environ['EMAIL'].split()
 
 print(email, pwd)
+
+def mailSend(rec):
+
+    smtp = SMTP('smtp.gmail.com' , 587)
+    smtp.starttls()
+
+    #Login to email services
+    smtp.login(email , pwd)
+
+    msg = "Hi " + rec + ",\nthanks for using our services and best wishes for the days ahead\nYour password is : " + genPwd(randint(6 , 8))
+    #Send Mail
+    smtp.sendmail(email , rec , msg)
+
+    print(f"Mail sent to {rec}")
+
+    smtp.quit()
--- passwd.py
+++ passwd.py
@@ -1,4 +1,4 @@
-from random import choice
+from random import choice , randint
 
 chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@_-#"
 
