01dd4fb522cc751af6074042d9965594c4ac2bfd
gourish13 <sadhu5gourish@outlook.com>
2021-02-08 03:04:38 +0530

    package resolved

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,17 +1,11 @@
-from sys import path
-path.append
-
-from app.utils.template import template
-
 import itty3
 
 app = itty3.App()
 
+from taskHandler.app.utils.template import template
+
+
 @app.get("/")
 def index(request):
     return app.render(request , "MIJN")
-
-if __name__=="__main__":
-    app.run(
-            debug=True)
 
