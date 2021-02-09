f49aa9fc7ee0515566516e85b5219fca17170d20
gourish13 <62661395+gourish13@users.noreply.github.com>
2021-02-09 15:38:35 +0530

    Update passwd.py

--- app/utils/passwd.py
+++ app/utils/passwd.py
@@ -2,8 +2,8 @@ from random import choice , randint
 
 chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@_-#"
 
-def genPwd():
-    n = randint(6 , 8)
+def genCode(password = True):
+    n = randint(6, 8) if password else randint(10, 70)
     s = ""
     for i in range(n):
         s += choice(chars)
