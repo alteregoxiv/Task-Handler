50f5ac81c5c9c5534a6fa4faefd24900a9df619d
gourish13 <sadhu5gourish@outlook.com>
2021-02-10 03:45:18 +0530

    added partial validation

--- app/statics/js/validate.js
+++ app/statics/js/validate.js
@@ -1,8 +1,35 @@
-function validateUserName() {
+function validateSignUpUserName(self) {
+        
+        if (!validateUserName(self, "invalid-user"))
+                return;
+}
+
+
+function validateEmail(self) {
 
 }
 
 
-function validateEmail() {
-
+function validateUserName(self, id) {
+        warning = document.getElementById(id)
+        if (self.value.split(' ').length > 1) {
+                showWarning(self, warning)
+                return false
+        }
+        hideWarning(self, warning)
+        return true;
 }
+
+
+function showWarning(self, warning) {
+        self.style.border = "2px solid red";
+        self.style.background = "rgba(240, 0, 0, .1)"
+        warning.innerHTML = 'username should be a single word';
+}
+
+
+function hideWarning(self, warning) {
+        self.style.border = "1px solid black";
+        self.style.background = "none"
+        warning.innerHTML = '';
+} 
