80af384b1bc983ba3a69ae79a5955aeae948a310
gourish13 <sadhu5gourish@outlook.com>
2021-02-10 11:23:40 +0530

    added signup route

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,10 +1,29 @@
+"""
+All routes of app is defined
+"""
+
 from taskHandler.app.utils.template import template
 
 import itty3
 
 app = itty3.App()
 
+
 @app.get("/")
 def index(request):
     return app.render(request , template("index.html"))
 
+
+@app.post("/signup")
+def signup(request):
+    return app.render(request, template("info.html"))
+
+
+@app.get("/validate")
+def validate(request):
+    is_valid = "invalid"
+    return app.render_json(
+        request,
+        data = dict(valid=is_valid),
+        content_type = "application/json"
+    )
