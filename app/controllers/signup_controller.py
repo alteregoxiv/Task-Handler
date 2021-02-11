2ab6e5e754efda5959dd4a072db66585c5687877
alteregoxiv <prasad.biswasxiv@gmail.com>
2021-02-11 19:21:42 +0000

    routes.py

--- app/controllers/signup_controller.py
+++ app/controllers/signup_controller.py
@@ -2,12 +2,19 @@
 Password generation, hashing and verfification
 """
 
-from taskHandler.app.utils.
+from taskHandler.app.utils.template import template
 from taskHandler.app.utils.hash import hashed, verfify
+from taskhandler.app.utils.psswd import genCode
+from taskhandler.app.utils.mail import mailSend
 
 def email_pwd(user, email):
-    pass
+    pwd = genCode()
+    mailSend(pwd)
+    return hash_pwd
 
 
 def verify_pwd(user, email, hash_pwd, pwd):
     pass
+
+if 'user' not in request.POST and 'email' not in request.POST:
+   hash_pwd  = email_pwd(user , email)
--- app/routes/routes.py
+++ app/routes/routes.py
@@ -3,7 +3,7 @@ All routes of app is defined
 """
 
 from taskHandler.app.utils.template import template
-from app.controllers.signupController import 
+from app.controllers.signupController import emailPwd
 
 import itty3
 
