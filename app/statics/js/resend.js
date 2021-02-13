a3bcdb759c767849d217e9a0f743ad116fabf7d3
gourish13 <sadhu5gourish@outlook.com>
2021-02-14 02:11:08 +0530

    finally count problem resolved

--- app/statics/js/resend.js
+++ app/statics/js/resend.js
@@ -2,13 +2,13 @@ resendPassword = (function() {
         let count = 2;
         return function(form) {
                 if (count > 0) {
+                        count--;
                         emailId = form.elements['email'].value
                         fetch('/resend-password?email=' + emailId)
                                 .then(function(data) { return data.json(); })
                                 .then(function(data) {
                                         form.elements['hash'].value = data['hash'];
                                         let button = form.elements[5];
-                                        count--;
                                         button.innerHTML = `Resend(${count})`;
                                         if (count === 0) button.disabled = true;
                                 })
