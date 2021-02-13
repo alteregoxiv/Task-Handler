0afe7941822ef4b7307563fb840b36b5ef75ce68
gourish13 <sadhu5gourish@outlook.com>
2021-02-14 02:00:54 +0530

    hash updation resolved

--- app/statics/js/resend.js
+++ app/statics/js/resend.js
@@ -1,19 +1,18 @@
 resendPassword = (function() {
         let count = 2;
         return function(form) {
-                emailId = form.elements['email'].value
-                fetch('/resend-password?email=' + emailId)
-                        .then(function(data) { return data.json(); })
-                        .then(function(data) {
-                                form.elements['hash'].value = data['hash'];
-                                let button = form.elements[5];
-                                if (count > 0) {
+                if (count > 0) {
+                        emailId = form.elements['email'].value
+                        fetch('/resend-password?email=' + emailId)
+                                .then(function(data) { return data.json(); })
+                                .then(function(data) {
+                                        form.elements['hash'].value = data['hash'];
+                                        let button = form.elements[5];
                                         count--;
                                         button.innerHTML = `Resend(${count})`;
-                                }
-                                if (count === 0) button.disabled = true;
-                        })
-                        .catch(function(err) { throw err; })
-                
+                                        if (count === 0) button.disabled = true;
+                                })
+                                .catch(function(err) { throw err; })
+                }
         }
 })();
