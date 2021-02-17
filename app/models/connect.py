e794b313866a3dcbc11d2185bff6d12d9acae580
gourish13 <sadhu5gourish@outlook.com>
2021-02-17 19:59:02 +0530

    added mysql connection string

--- app/models/connect.py
+++ app/models/connect.py
@@ -5,3 +5,6 @@ Connect to database
 from pydal import DAL
 
 db = DAL("sqlite://app/models/test.db")
+
+# DB_URI = "mysql://{}:{}@{}:{}/{}?set_encoding=utf8mb4".format()
+# db = DAL(DB_URI, pool_size=10)
